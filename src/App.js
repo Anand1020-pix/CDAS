1429e4a669671c74a9bb39ade017ff5578acd4e0
Anand1020-pix <59107989+Anand1020-pix@users.noreply.github.com>
2024-05-09 20:13:40 +0530

    01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'; // Import CSS file for styling
 
 function App() {
+  const [videoFiles, setVideoFiles] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  useEffect(() => {
+    const fetchVideoFiles = async () => {
+      try {
+        const response = await fetch('http://localhost:3010/detection');
+        const data = await response.json();
+        setVideoFiles(data);
+      } catch (error) {
+        console.error('Error fetching video files:', error);
+      }
+    };
+
+    fetchVideoFiles();
+  }, []);
+
+  const playVideo = (videoName) => {
+    setSelectedVideo(videoName);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="video-files">
+        <h2>CDAS</h2>
+        <ul>
+          {videoFiles.map((video, index) => (
+            <li key={index}>
+              <button onClick={() => playVideo(video)}>{video}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="video-player">
+        <h2></h2>
+        {selectedVideo && (
+          <video controls src={`http://localhost:3010/detection/${encodeURIComponent(selectedVideo)}`} />
+        )}
+      </div>
     </div>
   );
 }
