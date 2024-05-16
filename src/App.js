327668496db4c89c758699343e9c3e7bba47df46
Anand1020-pix <59107989+Anand1020-pix@users.noreply.github.com>
2024-05-16 19:27:54 +0530

    02

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import './App.css'; // Import CSS file for styling
+import ReactPlayer from 'react-player';
+import './App.css'; 
 
 function App() {
   const [videoFiles, setVideoFiles] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const [playing, setPlaying] = useState(true);
+  const [playbackRate, setPlaybackRate] = useState(1.0);
+  const playerRef = React.createRef();
 
   useEffect(() => {
     const fetchVideoFiles = async () => {
@@ -21,12 +25,29 @@ function App() {
 
   const playVideo = (videoName) => {
     setSelectedVideo(videoName);
+    setPlaying(true);
+  };
+
+  const togglePlay = () => {
+    setPlaying(!playing);
+  };
+
+  const handleRewind = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() - 0.4);
+  };
+
+  const handleFastForward = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() + 0.4);
+  };
+
+  const handleSpeedChange = (rate) => {
+    setPlaybackRate(rate);
   };
 
   return (
     <div className="container">
       <div className="video-files">
-        <h2>CDAS</h2>
+        <h2 >CDAS</h2>
         <ul>
           {videoFiles.map((video, index) => (
             <li key={index}>
@@ -36,10 +57,26 @@ function App() {
         </ul>
       </div>
       <div className="video-player">
-        <h2></h2>
+        <h2>Video Player</h2>
         {selectedVideo && (
-          <video controls src={`http://localhost:3010/detection/${encodeURIComponent(selectedVideo)}`} />
+          <ReactPlayer
+            ref={playerRef}
+            url={`http://localhost:3010/detection/${selectedVideo}`}
+            controls={true}
+            playing={playing}
+            playbackRate={playbackRate}
+            width='100%'
+          />
         )}
+        <div className="controls">
+          <button onClick={togglePlay}>{playing ? 'Pause' : 'Play'}</button>
+          <button onClick={handleRewind}>Rewind </button>
+          <button onClick={handleFastForward}>Fast Forward </button>
+          <button onClick={() => handleSpeedChange(0.5)}>0.5x</button>
+          <button onClick={() => handleSpeedChange(1)}>1x</button>
+          <button onClick={() => handleSpeedChange(1.5)}>1.5x</button>
+          <button onClick={() => handleSpeedChange(2)}>2x</button>
+        </div>
       </div>
     </div>
   );
